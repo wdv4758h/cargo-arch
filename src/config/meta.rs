3c060375cf114fe12ce27c2910e04734e2b95a77
Doug Goldstein <cardoe@cardoe.com>
2019-12-07 09:43:45 -0600

    ensure that the defaults of the arch section are populated
    Calling defaults without actually having a section is resulting in none
    of the fields being populated properly. Likely a serde bug.

--- src/config/meta.rs
+++ src/config/meta.rs
@@ -1,11 +1,19 @@
 //! Metadata for different platform's package
 
 use super::arch::CargoArch;
-
+use std::default::Default;
 
 /// data in `[package.metadata]` section
-#[derive(Clone, Debug, Default, Deserialize)]
+#[derive(Clone, Debug, Deserialize)]
 pub struct CargoMetadata {
     #[serde(default)]
     pub arch: CargoArch,
+}
+
+impl Default for CargoMetadata {
+    // if we do not decode an empty [arch] block then the defaults are
+    // not actually populated
+    fn default() -> Self {
+        toml::from_str("[arch]").unwrap()
+    }
 }
